fb65588d9c854cbae8e8b760b948148754444123
remyShift <remy.engassac@icloud.com>
2025-09-09 16:22:39 +0200

    init react project with tailwindcss

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  
+
+  return (
+    <div className='w-[100vw] h-[100vh] bg-primary'>
+      <h1 className='text-3xl font-bold underline text-black'>Hello world!</h1>
+    </div>
+  )
+}
