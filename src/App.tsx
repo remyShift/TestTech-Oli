581952dd22a5217a6b9bd7728f2218477c69d83d
remyShift <remy.engassac@icloud.com>
2025-09-09 16:43:37 +0200

    feat: start component navbar and add oli logo png from figma

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-export default function App() {
-  
+import Navbar from './components/navigation/Navbar'
 
+export default function App() {
   return (
     <div className='w-[100vw] h-[100vh] bg-primary'>
-      <h1 className='text-3xl font-bold underline text-black'>Hello world!</h1>
+      <Navbar />
+      <h1 className='text-3xl font-bold underline text-black'>Oli</h1>
     </div>
   )
 }
---
+++ src/components/navigation/Navbar.tsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+    return (
+        <div className='w-full h-10'>
+
+        </div>
+    )
+}
